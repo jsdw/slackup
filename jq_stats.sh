7efb42061a03d0611b651d3924eda28e0adf73f8
James Wilson <james.wilson@prowler.io>
2020-05-23 01:13:41 +0100

    Add word and char stats, arrays instead of select

--- jq_stats.sh
+++ jq_stats.sh
@@ -17,22 +17,28 @@ echo "Messages in channel:"
 jq "length" $FILE
 
 echo "Number of threads:"
-jq '[ .[].thread | select(. != null) ] | length' $FILE
+jq '[ .[].thread | arrays ] | length' $FILE
 
 echo "Number of messages in all threads:"
-jq '[ .[].thread | select(. != null) | .[] ] | length' $FILE
+jq '[ .[].thread | arrays | .[] ] | length' $FILE
 
 echo "Total messages:"
-jq '[ .[], (.[].thread | select(. != null) | .[]) ] | length' $FILE
+jq '[ .[], (.[].thread | arrays | .[]) ] | length' $FILE
 
 echo "The most messages in a single thread is:"
-jq '[ .[].thread | select(. != null) | length ] | max' $FILE
+jq '[ .[].thread | arrays | length ] | max' $FILE
 
 echo "Each person sent this number of messages (including in threads):"
-jq '[ .[], (.[].thread | select(. != null) | .[]) ] | [ group_by(.name) | .[] | { (.[0].name): length } ] | add' $FILE
+jq '[ .[], (.[].thread | arrays | .[]) ] | [ group_by(.name) | .[] | { (.[0].name): length } ] | add' $FILE
 
 echo "Number of threads started by each person:"
 jq '[ .[] | select(.thread) | .thread[0].name ] | [ group_by(.) | .[] | { (.[0]): length } ] | add' $FILE
 
 echo "Dates of the first and last message sent (including in threads):"
 jq '[ .[].thread[]?, .[] | .ts | tonumber | todate ] | {first_message: min, last_message: max}' $FILE
+
+echo "Each person typed this number of words (including in threads):"
+jq '[ .[], (.[].thread | arrays | .[]) | { name, words: [ .text | scan("['\''’a-zA-Z_-]+") ] | length } ] | [ group_by(.name) | .[] | { (.[0].name): [ .[] | .words ] | add } ] | add' $FILE
+
+echo "Each person typed this number of characters (including in threads):"
+jq '[ .[], (.[].thread | arrays | .[]) | { name, chars: .text | length } ] | [ group_by(.name) | .[] | { (.[0].name): [ .[] | .chars ] | add } ] | add' $FILE
